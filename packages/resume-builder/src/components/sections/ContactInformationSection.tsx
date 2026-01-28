3546a9492c72d3ebb7add7c8c3057fb1fc3930e2
Matt Strom <5853934+mattstrom@users.noreply.github.com>
2026-01-28 15:51:02 -0800

    feat: swap out lucide for phosphor icon for resume glpyhs

--- packages/resume-builder/src/components/sections/ContactInformationSection.tsx
+++ packages/resume-builder/src/components/sections/ContactInformationSection.tsx
@@ -1,15 +1,15 @@
-import clsx from 'clsx';
-import { type FC } from 'react';
-import { Url } from '../Url.tsx';
-import { useResume } from '../Resume.provider.tsx';
 import {
-	Mail,
-	Github,
+	Envelope,
+	GithubLogo,
 	Globe,
-	Linkedin,
+	LinkedinLogo,
 	MapPin,
 	Phone,
-} from 'lucide-react';
+} from '@phosphor-icons/react';
+import clsx from 'clsx';
+import { type FC } from 'react';
+import { useResume } from '../Resume.provider.tsx';
+import { Url } from '../Url.tsx';
 
 interface ContactInformationSectionProps {
 	className?: string;
@@ -24,45 +24,48 @@ export const ContactInformationSection: FC<ContactInformationSectionProps> = ({
 
 	return (
 		<address
-			className={clsx('contact-information flex flex-col gap-[0.1rem]', className)}
+			className={clsx(
+				'contact-information flex flex-col gap-[0.1rem]',
+				className,
+			)}
 			style={{
 				fontStyle: 'normal',
 			}}
 		>
 			<div className="contact-information-item location flex items-center gap-[5px]">
 				<div className="icon flex items-center m-auto">
-					<MapPin size={20} />
+					<MapPin size={24} weight="fill" />
 				</div>
 				<span>{contactInformation.location}</span>
 			</div>
 			<div className="contact-information-item phone-number flex items-center gap-[5px]">
 				<div className="icon flex items-center m-auto">
-					<Phone size={16} />
+					<Phone size={24} weight="fill" />
 				</div>
 				<a href={phoneNumberHref}>{contactInformation.phoneNumber}</a>
 			</div>
 			<div className="contact-information-item email flex items-center gap-[5px]">
 				<div className="icon flex items-center m-auto">
-					<Mail size={20} />
+					<Envelope size={24} />
 				</div>
 				<a href={emailHref}>{contactInformation.email}</a>
 			</div>
 			<div className="contact-information-item linkedin-profile flex items-center gap-[5px]">
 				<div className="icon flex items-center m-auto">
-					<Linkedin size={20} />
+					<LinkedinLogo size={24} weight="fill" />
 				</div>
 				<Url href={contactInformation.linkedInProfile} />
 			</div>
 			<div className="contact-information-item github-profile flex items-center gap-[5px]">
 				<div className="icon flex items-center m-auto">
-					<Github size={20} />
+					<GithubLogo size={24} weight="fill" />
 				</div>
 				<Url href={contactInformation.githubProfile} />
 			</div>
 			{contactInformation.personalWebsite && (
 				<div className="contact-information-item personal-website flex items-center gap-[5px]">
 					<div className="icon flex items-center m-auto">
-						<Globe size={20} />
+						<Globe size={24} />
 					</div>
 					<Url href={contactInformation.personalWebsite} />
 				</div>
