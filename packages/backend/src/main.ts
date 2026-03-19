97766e6a3658281e06c5590a33f93e639f293cd2
Matt Strom <strom.matt@gmail.com>
2026-03-19 11:37:12 -0700

    feat: restructure `docker-compose.yml` and introduce Dockerfiles for services
    - Add Dockerfiles for `packages/web` and `packages/backend`.
    - Update `docker-compose.yml` to include `backend`, `web`, and dependencies with mounted volumes.
    - Include `MONGODB_URI` environment variable in the `backend`.

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
+import { configuration } from '@/config';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
+	console.log(`Configuration:\n${configuration}`);
+
 	const app = await NestFactory.create(AppModule, { cors: true });
 	app.use(json({ limit: '10mb' }));
 	app.use(urlencoded({ extended: true, limit: '10mb' }));
