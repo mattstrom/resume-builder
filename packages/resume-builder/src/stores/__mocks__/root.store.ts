efe74e72412c3480b995d27fddf6cce3591eae79
Matt Strom <strom.matt@gmail.com>
2026-04-10 13:11:30 -0700

    chore: introduce storybook

---
+++ packages/resume-builder/src/stores/__mocks__/root.store.ts
@@ -0,0 +1,15 @@
+export class RootStore {
+	public inlineEditStore = {
+		isEditing(path: string): boolean {
+			return false;
+		},
+	};
+
+	public uiStateStore = {
+		isResumeEditable: true,
+	};
+
+	static getInstance() {
+		return new RootStore();
+	}
+}
